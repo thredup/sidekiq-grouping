4291fa33c477bcd147856bef1b055ff5bd6f505b
florrain <lorrain.florian@gmail.com>
2015-12-18 23:35:39 +0100

    [FIX] Specs

--- spec/modules/batch_spec.rb
+++ spec/modules/batch_spec.rb
@@ -5,108 +5,78 @@ describe Sidekiq::Grouping::Batch do
 
   context 'adding' do
     it 'must enqueue unbatched worker' do
-       RegularWorker.perform_async('bar')
-       expect(RegularWorker).to have_enqueued_job('bar')
+       RegularWorker.perform_async('foo_option', 'bar')
+       expect(RegularWorker).to have_enqueued_job('foo_option', 'bar')
     end
 
     it 'must not enqueue batched worker' do
-      BatchedSizeWorker.perform_async('bar')
-      expect_batch(BatchedSizeWorker, 'batched_size')
-    end
-
-    it 'must not enqueue batched worker' do
-      BatchedIntervalWorker.perform_async('bar')
-      expect_batch(BatchedIntervalWorker, 'batched_interval')
-    end
-
-    it 'must not enqueue batched worker' do
-      BatchedBothWorker.perform_async('bar')
-      expect_batch(BatchedBothWorker, 'batched_both')
+      BatchedSizeWorker.perform_async('foo_option', 'bar')
+      expect_batch(BatchedSizeWorker, 'batched_size', 'foo_option')
     end
   end
 
   context 'checking if should flush' do
-    it 'must flush if limit exceeds for limit worker' do
-      batch = subject.new(BatchedSizeWorker.name, 'batched_size')
+    it 'should check this scenario' do
+      batch = subject.new(BatchedSizeWorker.name, 'batched_size', 'foo_option')
 
+      # empty batch, the dates are not set.
       expect(batch.could_flush?).to be_falsy
-      BatchedSizeWorker.perform_async('bar')
+      BatchedSizeWorker.perform_async('foo_option', 'bar')
+      # non empty batch, dates are initiated but the batch is not flushed
       expect(batch.could_flush?).to be_falsy
-      4.times { BatchedSizeWorker.perform_async('bar') }
-      expect(batch.could_flush?).to be_truthy
-    end
-
-    it 'must flush if limit exceeds for both worker' do
-      batch = subject.new(BatchedBothWorker.name, 'batched_both')
-
-      expect(batch.could_flush?).to be_falsy
-      BatchedBothWorker.perform_async('bar')
-      expect(batch.could_flush?).to be_falsy
-      4.times { BatchedBothWorker.perform_async('bar') }
-      expect(batch.could_flush?).to be_truthy
-    end
-
-    it 'must flush if limit okay but time came' do
-      batch = subject.new(BatchedIntervalWorker.name, 'batched_interval')
-
-      expect(batch.could_flush?).to be_falsy
-      BatchedIntervalWorker.perform_async('bar')
-      expect(batch.could_flush?).to be_falsy
-      expect(batch.size).to eq(1)
-
-      Timecop.travel(2.hours.since)
-
+      Timecop.travel(1.minute.since)
+      # we are 1min from the last check with a non empty batch => flushable
       expect(batch.could_flush?).to be_truthy
     end
   end
 
   context 'flushing' do
-    it 'must put wokrer to queue on flush' do
-      batch = subject.new(BatchedSizeWorker.name, 'batched_size')
+    it 'must put worker to queue on flush' do
+      batch = subject.new(BatchedSizeWorker.name, 'batched_size', 'foo_option')
 
       expect(batch.could_flush?).to be_falsy
-      10.times { |n| BatchedSizeWorker.perform_async("bar#{n}") }
-      batch.flush
-      expect(BatchedSizeWorker).to have_enqueued_job([["bar0"], ["bar1"]])
-      expect(batch.size).to eq(7)
+      10.times { |n| BatchedSizeWorker.perform_async('foo_option', "bar#{n}") }
+      batch.flush(2)
+      expect(BatchedSizeWorker).to have_enqueued_job({ "queue_option" => "foo_option", "chunks" => [[["foo_option", "bar0"], ["foo_option", "bar1"]]] })
+      expect(batch.size).to eq(8)
     end
   end
 
   context 'with similar args' do
     context 'option batch_unique = true' do
       it 'enqueues once' do
-        batch = subject.new(BatchedUniqueArgsWorker.name, 'batched_unique_args')
-        3.times { BatchedUniqueArgsWorker.perform_async('bar', 1) }
+        batch = subject.new(BatchedUniqueArgsWorker.name, 'batched_unique_args', 'foo_option')
+        3.times { BatchedUniqueArgsWorker.perform_async('foo_option', 'bar', 1) }
         expect(batch.size).to eq(1)
       end
 
       it 'enqueues once each unique set of args' do
-        batch = subject.new(BatchedUniqueArgsWorker.name, 'batched_unique_args')
-        3.times { BatchedUniqueArgsWorker.perform_async('bar', 1) }
-        6.times { BatchedUniqueArgsWorker.perform_async('baz', 1) }
-        3.times { BatchedUniqueArgsWorker.perform_async('bar', 1) }
-        2.times { BatchedUniqueArgsWorker.perform_async('baz', 3) }
-        7.times { BatchedUniqueArgsWorker.perform_async('bar', 1) }
+        batch = subject.new(BatchedUniqueArgsWorker.name, 'batched_unique_args', 'foo_option')
+        3.times { BatchedUniqueArgsWorker.perform_async('foo_option', 'bar', 1) }
+        6.times { BatchedUniqueArgsWorker.perform_async('foo_option', 'baz', 1) }
+        3.times { BatchedUniqueArgsWorker.perform_async('foo_option', 'bar', 1) }
+        2.times { BatchedUniqueArgsWorker.perform_async('foo_option', 'baz', 3) }
+        7.times { BatchedUniqueArgsWorker.perform_async('foo_option', 'bar', 1) }
         expect(batch.size).to eq(3)
       end
 
       context 'flushing' do
 
         it 'works' do
-          batch = subject.new(BatchedUniqueArgsWorker.name, 'batched_unique_args')
-          2.times { BatchedUniqueArgsWorker.perform_async('bar', 1) }
-          2.times { BatchedUniqueArgsWorker.perform_async('baz', 1) }
-          batch.flush
+          batch = subject.new(BatchedUniqueArgsWorker.name, 'batched_unique_args', 'foo_option')
+          2.times { BatchedUniqueArgsWorker.perform_async('foo_option' 'bar', 1) }
+          2.times { BatchedUniqueArgsWorker.perform_async('foo_option', 'baz', 1) }
+          batch.flush(batch.size)
           expect(batch.size).to eq(0)
         end
 
         it 'allows to enqueue again after flush' do
-          batch = subject.new(BatchedUniqueArgsWorker.name, 'batched_unique_args')
-          2.times { BatchedUniqueArgsWorker.perform_async('bar', 1) }
-          2.times { BatchedUniqueArgsWorker.perform_async('baz', 1) }
-          batch.flush
-          BatchedUniqueArgsWorker.perform_async('bar', 1)
-          BatchedUniqueArgsWorker.perform_async('baz', 1)
+          batch = subject.new(BatchedUniqueArgsWorker.name, 'batched_unique_args', 'foo_option')
+          2.times { BatchedUniqueArgsWorker.perform_async('foo_option', 'bar', 1) }
+          2.times { BatchedUniqueArgsWorker.perform_async('foo_option', 'baz', 1) }
+          batch.flush(batch.size)
+          BatchedUniqueArgsWorker.perform_async('foo_option', 'bar', 1)
+          BatchedUniqueArgsWorker.perform_async('foo_option', 'baz', 1)
           expect(batch.size).to eq(2)
         end
       end
@@ -115,22 +85,22 @@ describe Sidekiq::Grouping::Batch do
 
     context 'batch_unique is not specified' do
       it 'enqueues all' do
-        batch = subject.new(BatchedSizeWorker.name, 'batched_size')
-        3.times { BatchedSizeWorker.perform_async('bar', 1) }
+        batch = subject.new(BatchedSizeWorker.name, 'batched_size', 'foo_option')
+        3.times { BatchedSizeWorker.perform_async('foo_option', 'bar', 1) }
         expect(batch.size).to eq(3)
       end
     end
   end
 
   private
-  def expect_batch(klass, queue)
-    expect(klass).to_not have_enqueued_job('bar')
-    batch = subject.new(klass.name, queue)
+  def expect_batch(klass, queue, queue_option)
+    expect(klass).to_not have_enqueued_job('foo_option', 'bar')
+    batch = subject.new(klass.name, queue, queue_option)
     stats = subject.all
     expect(batch.size).to eq(1)
     expect(stats.size).to eq(1)
     expect(stats.first.worker_class).to eq(klass.name)
     expect(stats.first.queue).to eq(queue)
-    expect(batch.pluck).to eq [['bar']]
+    expect(batch.pluck(batch.size)).to eq [['foo_option', 'bar']]
   end
 end
--- spec/support/test_workers.rb
+++ spec/support/test_workers.rb
@@ -8,27 +8,7 @@ end
 class BatchedSizeWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :batched_size, batch_flush_size: 3, batch_size: 2
-
-  def perform(foo)
-  end
-end
-
-class BatchedIntervalWorker
-  include Sidekiq::Worker
-
-  sidekiq_options queue: :batched_interval, batch_flush_interval: 3600
-
-  def perform(foo)
-  end
-end
-
-class BatchedBothWorker
-  include Sidekiq::Worker
-
-  sidekiq_options(
-    queue: :batched_both, batch_flush_interval: 3600, batch_flush_size: 3
-  )
+  sidekiq_options grouping: true, queue: :batched_size, max_calls_per_min: 2
 
   def perform(foo)
   end
@@ -38,7 +18,7 @@ class BatchedUniqueArgsWorker
   include Sidekiq::Worker
 
   sidekiq_options(
-    queue: :batched_unique_args, batch_flush_size: 3, batch_unique: true
+    grouping: true, queue: :batched_unique_args, max_calls_per_min: 3, batch_unique: true
   )
 
   def perform(foo)
