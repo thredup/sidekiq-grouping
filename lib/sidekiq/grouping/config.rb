fc4aa07003ced6e73fe6464e5164adb479216621
florrain <lorrain.florian@gmail.com>
2015-12-21 16:00:43 +0100

    [UPDATE] Keeping batch_flush_interval in the config.

--- lib/sidekiq/grouping/config.rb
+++ lib/sidekiq/grouping/config.rb
@@ -3,7 +3,7 @@ module Sidekiq
     module Config
       include ActiveSupport::Configurable
 
-      config_accessor :enabled, :poll_interval, :default_flush_interval, :max_records_per_call, :max_calls_per_minute, :lock_ttl
+      config_accessor :enabled, :poll_interval, :batch_flush_interval, :max_records_per_call, :max_calls_per_minute, :lock_ttl
 
       ### Default values ###
       self.config.enabled = true
@@ -12,7 +12,7 @@ module Sidekiq
       self.config.poll_interval = 3
 
       # Flush the queue every x seconds
-      self.config.default_flush_interval = 60
+      self.config.batch_flush_interval = 60
 
       # How many records max should be grouped together
       self.config.max_records_per_call = 200
